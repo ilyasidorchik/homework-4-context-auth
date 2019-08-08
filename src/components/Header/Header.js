092ac341d5270364496e5b7255925da2e1b8aad6
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-08 11:51:49 +0300

    Finishes Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,32 @@
 import React, { PureComponent } from 'react';
 import './Header.css';
+import { AuthConsumer } from '../../contexts/Auth';
 
 class Header extends PureComponent {
   render() {
-    return ('empty');
+    return (
+      <AuthConsumer>
+        {({ isAuthorised, email, logout }) =>
+          isAuthorised
+          ? (
+            <div className="header__content">
+              <div className="header-menu">
+                <p className="header-menu__email header-email t-header-email">
+                  { email }
+                </p>
+                <Button
+                  onClick={ logout }
+                  className="header-menu__button t-logout"
+                >
+                  Выйти
+                </Button>
+              </div>
+            </div>
+          )
+          : null
+        }
+      </AuthConsumer>
+    );
   }
 }
 
