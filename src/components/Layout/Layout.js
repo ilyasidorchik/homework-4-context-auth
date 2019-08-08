fe5e99daae2cb918fb98647ec42bc29aca81d158
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-08 12:24:56 +0300

    Finishes Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,47 @@
 import React, { PureComponent } from 'react';
+import SectionTitle from '../SectionTitle';
 import './Layout.css';
 
 class Layout extends PureComponent {
+  renderHeader(HeaderChild) {
+    if (HeaderChild) {
+      return (
+        <header className="header">
+          <SectionTitle className="header__title">Header</SectionTitle>
+          <HeaderChild />
+        </header>
+      );
+    }
+  }
+
+  renderFooter(FooterChild) {
+    if (FooterChild) {
+      return (
+        <footer className="footer">
+          <SectionTitle className="footer__title">Footer</SectionTitle>
+          <FooterChild />
+        </footer>
+      );
+    }
+  }
+
   render() {
-    return 'empty';
+    const { header, footer, children } = this.props;
+    const renderHeader = this.renderHeader;
+    const renderFooter = this.renderFooter;
+
+    return (
+      <div>
+        { renderHeader(header) }
+        <main
+          className={ `main ${header && 'main--with-header'} ${footer && 'main--with-footer'}` }
+        >
+          <SectionTitle className="main__title" children="Main" />
+          { children }
+        </main>
+        { renderFooter(footer) }
+      </div>
+    );
   }
 }
 
